5c6e5823e9e2151b628df1c7b0eb98c608dec57f
RoboMWM <robomwm@users.noreply.github.com>
2016-02-12 01:01:16 -0800

    Add ItemSpawnEvent detector
    will now retain previous events or somethin'

--- src/main/java/intcraft/canceldetect/PvPCancelDetector.java
+++ src/main/java/intcraft/canceldetect/PvPCancelDetector.java
@@ -3,6 +3,7 @@ package intcraft.canceldetect;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
@@ -13,6 +14,7 @@ import intcraft.canceldetect.CancellationDetector.CancelListener;
 public class PvPCancelDetector extends JavaPlugin implements Listener
 {
     private CancellationDetector<EntityDamageEvent> detector = new CancellationDetector<EntityDamageEvent>(EntityDamageEvent.class);
+    private CancellationDetector<ItemSpawnEvent> ItemSpawndetector = new CancellationDetector<ItemSpawnEvent>(ItemSpawnEvent.class);
 
     @Override
     public void onEnable()
@@ -24,7 +26,16 @@ public class PvPCancelDetector extends JavaPlugin implements Listener
             @Override
             public void onCancelled(Plugin plugin, EntityDamageEvent event)
             {
-                System.out.println("cancelled by " + plugin);
+                System.out.println("EntityDamage cancelled by " + plugin);
+            }
+        });
+
+        ItemSpawndetector.addListener(new CancelListener<ItemSpawnEvent>()
+        {
+            @Override
+            public void onCancelled(Plugin plugin, ItemSpawnEvent event)
+            {
+                System.out.println("ItemSpawn cancelled by " + plugin);
             }
         });
     }
